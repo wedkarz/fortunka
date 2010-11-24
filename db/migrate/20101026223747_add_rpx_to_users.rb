6d54b84ca453ee9e7e2f7d9b8a5f122483855685
Artur Rybak <artur.rybak@gmail.com>
2010-11-24 16:26:53 +0100

    poprawki dla GITa - i jest Git :)

--- db/migrate/20101026223747_add_rpx_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddRpxToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :rpx_indentifier, :string
-  end
-
-  def self.down
-    remove_column :users, :rpx_indentifier
-  end
-end
